8fde22d55dd675e74cc34d1576ceabe17d0d60dc
CamiloJala22 <Camilo.Valdes@fundacion-jala.org>
2023-07-14 12:27:42 -0500

    First-Class-Homework

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -5,19 +5,26 @@ from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Spaceship(Sprite):
     def __init__(self):
+        # Tamaño de la nave
+        self.width_spaceship = 40 #   
+        self.height_spaceship = 60 # 
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image, (40, 60))
+        self.image = pygame.transform.scale(self.image, (self.width_spaceship, self.height_spaceship)) #
         self.rect = self.image.get_rect()
-        self.rect.x = (SCREEN_WIDTH // 2) - 40
+        self.rect.x = (SCREEN_WIDTH // 2) - self.width_spaceship #
         self.rect.y = 500
     
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
-            if self.rect.left > 0:
+            if self.rect.left > -self.width_spaceship: #
                 self.rect.x -= 10
+            else:
+                self.rect.x = SCREEN_WIDTH
         elif user_input[pygame.K_RIGHT]:
-            if self.rect.right < SCREEN_WIDTH:
+            if self.rect.right < SCREEN_WIDTH + self.width_spaceship: #
                 self.rect.x += 10
+            else:
+                self.rect.x = -self.width_spaceship #
         elif user_input[pygame.K_UP]:
             if self.rect.y > SCREEN_HEIGHT // 2:
                 self.rect.y -= 10
