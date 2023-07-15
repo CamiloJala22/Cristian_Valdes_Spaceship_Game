20088cda3c051c55f5875e210e732f88201b79fb
CamiloJala22 <Camilo.Valdes@fundacion-jala.org>
2023-07-15 11:00:06 -0500

    Second_Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,36 +1,48 @@
 import pygame
 from pygame.sprite import Sprite
 
-from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT, SHIP_WIDTH, SHIP_HEIGHT
 
 class Spaceship(Sprite):
+    X_POS = (SCREEN_WIDTH // 2) - SHIP_WIDTH
+    Y_POS = 500
+    SHIP_SPACE = 10
     def __init__(self):
         # Tamaño de la nave
-        self.width_spaceship = 40 #   
-        self.height_spaceship = 60 # 
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image, (self.width_spaceship, self.height_spaceship)) #
+        self.image = pygame.transform.scale(self.image, (SHIP_WIDTH, SHIP_HEIGHT)) #
         self.rect = self.image.get_rect()
-        self.rect.x = (SCREEN_WIDTH // 2) - self.width_spaceship #
-        self.rect.y = 500
+        self.rect.x = self.X_POS #
+        self.rect.y = self.Y_POS
     
     def update(self, user_input):
+        # Cuando se esconde la nave si se mueve hacia arriba o abajo, desaparece la nave
         if user_input[pygame.K_LEFT]:
-            if self.rect.left > -self.width_spaceship: #
-                self.rect.x -= 10
-            else:
-                self.rect.x = SCREEN_WIDTH
-        elif user_input[pygame.K_RIGHT]:
-            if self.rect.right < SCREEN_WIDTH + self.width_spaceship: #
-                self.rect.x += 10
-            else:
-                self.rect.x = -self.width_spaceship #
-        elif user_input[pygame.K_UP]:
-            if self.rect.y > SCREEN_HEIGHT // 2:
-                self.rect.y -= 10
-        elif user_input[pygame.K_DOWN]:
-            if self.rect.y < SCREEN_HEIGHT - 70:
-                self.rect.y += 10
+            self.move_left()
+        if user_input[pygame.K_RIGHT]:
+            self.move_right()
+        if user_input[pygame.K_UP]:
+            self.move_up()
+        if user_input[pygame.K_DOWN]:
+            self.move_down()
+
+    def move_left(self):
+        self.rect.x -= self.SHIP_SPACE
+        if self.rect.left == -SHIP_WIDTH: 
+            self.rect.x = SCREEN_WIDTH
+    
+    def move_right(self):
+        self.rect.x += self.SHIP_SPACE
+        if self.rect.right == SCREEN_WIDTH + SHIP_WIDTH: 
+            self.rect.x = -SHIP_WIDTH
+
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT // 2:
+            self.rect.y -= self.SHIP_SPACE
+
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT - 70:
+            self.rect.y += self.SHIP_SPACE
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
